9029a985373ee890fee84554eb9d0d9996b64a88
didrudcks <rudcksdid@naver.com>
2020-12-18 11:17:38 +0900

    jake

---
+++ src/board/Controller.java
@@ -0,0 +1,47 @@
+package board;
+
+import java.io.IOException;
+import java.util.ArrayList;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import board.article.Article;
+import board.article.ArticleDao;
+
+
+@WebServlet("/Controller")
+public class Controller extends HttpServlet {
+	
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		
+		response.setCharacterEncoding("UTF-8");
+		
+		ArticleDao articleDao = new ArticleDao();
+		ArrayList<Article> articles = articleDao.getArticles();
+		
+		String action = request.getParameter("action");
+		
+		request.setAttribute("myData", articles);
+		
+		String dest = "";
+		
+		if(action.equals("list1.jsp"))
+		{
+			dest = "/list1.jsp";
+		}
+		else
+		{
+			dest = "/list2.jsp";
+		}
+		
+		RequestDispatcher rd = request.getRequestDispatcher(dest);
+		rd.forward(request, response);
+		
+	}
+
+}
